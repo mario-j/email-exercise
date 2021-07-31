0a0806c1c0f5acf6caa47e2a91a24ad37a9d6040
Mario Jordan <Mario.Jordan@kumandgo.com>
2021-07-30 23:36:09 -0500

    Retrieve and parse emails from test gmail account

--- app.js
+++ app.js
@@ -17,3 +17,63 @@ let handleRequest = (request, response) => {
 };
 
 http.createServer(handleRequest).listen(8000)
+
+//#######################################################
+
+var Imap = require('imap'),
+    inspect = require('util').inspect;
+
+const simpleParser = require('mailparser').simpleParser;
+
+console.log("parse", simpleParser);
+
+var imap = new Imap({
+    user: 'donaldmcgriddle@gmail.com',
+    password: 'F00tball4!',
+    host: 'imap.gmail.com',
+    port: 993,
+    tls: true,
+    tlsOptions: {
+        servername: 'imap.gmail.com',
+    }
+})
+
+imap.once('ready', function () { 
+    console.log("ready");
+    openInbox(function (err, box) { 
+        var f = imap.seq.fetch('1:3', {
+            bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+            struct: true
+        });
+
+        f.on('message', function (msg, seqno) {
+            console.log('Message #%d', seqno);
+            msg.on('body', async function (stream, info) {
+                var parsed = await simpleParser(stream)
+                console.log("parsed", parsed);
+            });
+        })
+        // this will occurs when any error occurs while fetching emails
+        f.on('error', function (err) {
+            console.log(err)
+        })
+
+        f.on('end', function () {
+
+        })
+    })
+})
+
+imap.once('error', function (err) {
+    console.log(err);
+});
+
+imap.once('end', function () { 
+    console.log('Connection ended');
+});
+
+imap.connect()
+
+function openInbox(cb) {
+    imap.openBox('INBOX', true, cb);
+}
