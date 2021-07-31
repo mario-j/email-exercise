e2a30d09b42f78c47ce0eab7624d5d717aa27625
Mario Jordan <Mario.Jordan@kumandgo.com>
2021-07-31 01:24:09 -0500

    Add scaffolding for input fields

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Server {
+  value: string;
+  viewValue: string;
+}
+
+interface Encryption {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'my-app';  
+  
+  servers: Server[] = [
+    {value: 'server-0', viewValue: 'Server 1'},
+    {value: 'server-1', viewValue: 'Server 2'},
+    {value: 'server-2', viewValue: 'Server 3'}
+  ];
+
+  encryptions: Encryption[] = [
+    {value: 'encryption-0', viewValue: 'Encryption 1'},
+    {value: 'encryption-1', viewValue: 'Encryption 2'},
+    {value: 'encryption-2', viewValue: 'Encryption 3'}
+  ];
 }
